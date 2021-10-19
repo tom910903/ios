7b0b499a98b459439dad0c1f3f1f84190e99a8b4
daeyoung lee <tom910903@gmail.com>
2021-10-19 21:48:10 +0900

    init

---
+++ calculator/Shared/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  Shared
+//
+//  Created by daeyoung lee on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
