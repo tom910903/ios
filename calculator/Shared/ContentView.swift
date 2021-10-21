edbe589942bf0f8c84313821d88df6f6a807d119
daeyoung.lee <tom910903@gmail.com>
2021-10-21 21:25:26 +0900

    init Layout

--- calculator/Shared/ContentView.swift
+++ calculator/Shared/ContentView.swift
@@ -8,9 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["AC","*","%","/"],
+        ["7","8","9","X"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".",".","="],
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom){
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("42").font(.system(size: 64))
+                        .foregroundColor(.white)
+                }.padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12){
+                        ForEach(row, id: \.self) { button in
+                            Text(button).font(.system(size: 32))
+                                .frame(width: self.buttonWidth(), height: self.buttonWidth())
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(40)
+                        }
+                    }
+                }
+            }.padding(.bottom)
+        }
+    }
+    
+    func buttonWidth()->CGFloat{
+        return (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
